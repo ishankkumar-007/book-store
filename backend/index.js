63e1fa437fdfd0422d8948955fcee3a1937df01c
Ishank Kumar <ishankkumar123@gmail.com>
2024-06-15 18:36:42 +0530

    Create first http route using get method

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,12 @@
-import express from "express";
+import express, { response } from "express";
 import { port } from "./config.js"
 
 const app = express();
+
+app.get('/', (req, res) => {
+    console.log(req)
+    return res.status(234).send('Welcome to THE STORE')
+});
 
 app.listen(port, () => {
     console.log(`App is listening to port: ${port}`)
