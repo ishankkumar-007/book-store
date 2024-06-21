48ccf1b1f310b762d91b421a39596b1c8715516b
Ishank Kumar <ishankkumar123@gmail.com>
2024-06-21 15:37:03 +0530

    add middleware for handling cors policy

--- backend/index.js
+++ backend/index.js
@@ -3,11 +3,24 @@ import { port, mongoDBURL } from "./configs.js";
 import mongoose, { mongo } from "mongoose";
 import { Book } from './models/bookModel.js';
 import booksRoute from './routes/booksRoute.js';
+import cors from 'cors';
 
 const app = express();
 
 // Middleware for parsing req body
 app.use(express.json());
+
+// Middleware for handling CORS POLICY
+// // Option 1: Allow All Origins wiht defautl of cors(*)
+// app.use(cors());
+// Option 2: Allow Custom Origins   
+app.use(
+    cors({
+        origin: 'http://localhost:3000',
+        methods: ['GET', 'POST', 'PUT', 'DELETE'],
+        allowedHeaders: ['Content-type'],
+    })
+);
 
 app.get('/', (req, res) => {
     console.log(req)
